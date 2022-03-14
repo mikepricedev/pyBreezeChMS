d7dc8a2f82537b4b7b2b2fdb84adab6e7c4fac46
Michael Price <mikepricedev@gmail.com>
2022-03-13 19:00:11 -0600

    feat:  Moved LOGGER_NAME const to __init__.py

--- breeze/breeze.py
+++ breeze/breeze.py
@@ -40,8 +40,6 @@ class BreezeError(Exception):
 MAX_EVENTS_LIMIT = 1000
 MAX_ACCOUNT_LOG_LIMIT = 3000
 
-LOGGER_NAME = __name__
-
 
 class BreezeApi(object):
     """A wrapper for the Breeze REST API."""
